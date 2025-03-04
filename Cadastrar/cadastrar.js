e98918a6c7ccc4aaf3c9659986aff1bf982a9171
Metalink10 <lmlira29@gmail.com>
2025-03-04 14:02:00 -0300

    Add files via upload

---
+++ Cadastrar/cadastrar.js
@@ -0,0 +1,33 @@
+const txtNome = document.getElementById("errorNome");
+const txtSenha = document.getElementById("errorSenha");
+const txtEmail = document.getElementById("errorEmail");
+const msg = document.getElementById("msgSucesso");
+
+function cadastrarUsuarios() {
+  const nome = document.getElementById("nome").value;
+  const senha = document.getElementById("senha").value;
+  const email = document.getElementById("email").value;
+
+  
+
+  if (nome  && senha && email ) {
+    let listaUser = JSON.parse(localStorage.getItem("listaUser") || "[]");
+    listaUser.push({
+      nomeCad: nome,
+      emailCad: email,
+      senhaCad: senha
+    });
+
+    localStorage.setItem('listaUser', JSON.stringify(listaUser));
+
+    msg.setAttribute("style", "display: block")
+
+    setTimeout(() => {
+      window.location.href = 'https://tela-de-login-brown-five.vercel.app/';
+    },2000)
+  }else if(nome === "" && email ==="" && senha === "" ) {
+    txtNome.innerHTML =  "Preencha o campo";
+    txtEmail.innerHTML =  "Preencha o campo";
+    txtSenha.innerHTML =  "Preencha o campo";
+  }
+};
